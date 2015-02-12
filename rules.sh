05f32097fa9c19f64377db40572d8d83a2e6c8d1
georgi123 <georgi.dimitrov@gdmfx.com>
2015-02-12 12:28:34 +0200

    Update rules.sh

--- rules.sh
+++ rules.sh
@@ -2,8 +2,12 @@
 #this rules may diferen for 
 #a different cases
 #this will change when is a nessery
-ufw allow
-ufw allow port 22
-ufw allow port 80
-ufw allow port 443
-
+ufw allow  1:21000/tcp
+ufw allow  21001:21049/tcp
+ufw allow  21051:65530/tcp
+ufw allow from 0.0.0.0/0 proto udp
+ufw allow proto tcp from 172.25.48.0/24 to any port 21000
+ufw allow proto tcp from 172.25.48.0/24 to any port 21050
+ufw allow proto tcp from 195.238.163.135 to any port 21000
+ufw allow proto tcp from 195.238.163.135 to any port 21050
+ufw enable
